c606c6959aaed31cb07f7462436d2b37d9815022
Derek Parker <parkerderek86@gmail.com>
2020-04-08 15:41:58 -0700

    Prevent "double drop" of pty master FD

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::error::Error;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
-use std::os::unix::io::{AsRawFd, FromRawFd, RawFd};
+use std::os::unix::io::{FromRawFd, IntoRawFd, RawFd};
 
 use nix::fcntl::OFlag;
 use nix::pty;
@@ -10,6 +10,7 @@ use nix::sys::termios;
 
 use mio::unix::SourceFd;
 use mio::{Events, Interest, Poll, Token};
+use nix::pty::PtyMaster;
 
 const STDIN: Token = Token(0);
 const PTY_MASTER: Token = Token(1);
@@ -27,11 +28,16 @@ fn term_set_raw(fd: RawFd, termios: &mut termios::Termios) -> Result<(), nix::Er
 }
 
 /// Proxies between stdin of this process to the master terminal device.
-fn proxy_term(stdin: RawFd, pty_master: RawFd) -> Result<(), Box<dyn Error>> {
+fn proxy_term(stdin: RawFd, pty_master: PtyMaster) -> Result<(), Box<dyn Error>> {
     let mut poll = Poll::new()?;
     let mut events = Events::with_capacity(128);
     let mut buf: [u8; 2048] = [0; 2048];
-    let fpty_master: &mut File = unsafe { &mut File::from_raw_fd(pty_master) };
+
+    // Use IntoRawFd here because we want to transfer ownership of the
+    // file descriptor to this function. This prevents a "double drop"
+    // when main returns.
+    let pty_master_fd = pty_master.into_raw_fd();
+    let fpty_master: &mut File = unsafe { &mut File::from_raw_fd(pty_master_fd) };
 
     // We have to use a raw handle to the stdout stream due to the
     // lock that is put on it if we get a reference via io::stdout().
@@ -44,8 +50,11 @@ fn proxy_term(stdin: RawFd, pty_master: RawFd) -> Result<(), Box<dyn Error>> {
         .register(&mut SourceFd(&stdin), STDIN, Interest::READABLE)?;
 
     // Register PTY master, wait for it to be readable.
-    poll.registry()
-        .register(&mut SourceFd(&pty_master), PTY_MASTER, Interest::READABLE)?;
+    poll.registry().register(
+        &mut SourceFd(&pty_master_fd),
+        PTY_MASTER,
+        Interest::READABLE,
+    )?;
 
     loop {
         // Poll for events, blocking until we get an event.
@@ -53,6 +62,9 @@ fn proxy_term(stdin: RawFd, pty_master: RawFd) -> Result<(), Box<dyn Error>> {
 
         // Process each event.
         for event in events.iter() {
+            if event.is_read_closed() {
+                return Ok(());
+            }
             match event.token() {
                 STDIN => {
                     let n = io::stdin().read(&mut buf)?;
@@ -98,5 +110,5 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Opened new PTY device: {}", pty_pair.slave_name);
 
     // Proxy between our stdin device and the PTY master device.
-    proxy_term(stdin, pty_pair.master.as_raw_fd())
+    proxy_term(stdin, pty_pair.master)
 }
